6350bd03665202471fb083ffc2eeb03cdbc40af9
beomobile <60918933+beomobile@users.noreply.github.com>
2023-05-10 17:07:42 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -41,45 +41,44 @@ if not client.is_user_authorized():
     os.system('clear')
     banner()
     client.sign_in(phone, input(gr+'[+] Enter the code: '+re))
- 
+
 os.system('clear')
 banner()
 chats = []
 last_date = None
-chunk_size = 200
 groups=[]
- 
+
 result = client(GetDialogsRequest(
              offset_date=last_date,
              offset_id=0,
              offset_peer=InputPeerEmpty(),
-             limit=chunk_size,
+             limit=100,
              hash = 0
          ))
 chats.extend(result.chats)
- 
+
 for chat in chats:
     try:
         if chat.megagroup== True:
             groups.append(chat)
     except:
         continue
- 
+
 print(gr+'[+] Choose a group to scrape members :'+re)
 i=0
 for g in groups:
     print(gr+'['+cy+str(i)+']' + ' - ' + g.title)
     i+=1
- 
+
 print('')
 g_index = input(gr+"[+] Enter a Number : "+re)
 target_group=groups[int(g_index)]
- 
+
 print(gr+'[+] Fetching Members...')
 time.sleep(1)
 all_participants = []
-all_participants = client.get_participants(target_group, aggressive=True)
- 
+all_participants = client.get_participants(target_group, aggressive=True, limit=None)
+
 print(gr+'[+] Saving In file...')
 time.sleep(1)
 with open("members.csv","w",encoding='UTF-8') as f:
